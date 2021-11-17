b42f0f43db7633d75bb4b1ec16b4c1d93b3d68a6
Sheila Leveille <sbleveille@gmail.com>
2021-11-17 09:48:04 +0100

    ARCH: Initial app with geocoded map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,9 +22,11 @@ ActiveStorage.start()
 import "bootstrap";
 
 // Internal imports, e.g:
-// import { initSelect2 } from '../components/init_select2';
+import { initMapbox } from '../plugins/init_mapbox';
 
 document.addEventListener('turbolinks:load', () => {
+
   // Call your functions here, e.g:
   // initSelect2();
+  initMapbox();
 });
